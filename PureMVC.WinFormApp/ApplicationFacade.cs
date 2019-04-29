525c688d88e925ec8d9e4e4f37f71bfbe9b84299
Alex Chen <xibeiwind@126.com>
2019-04-29 10:43:06 +0800

    add TestApp

---
+++ PureMVC.WinFormApp/ApplicationFacade.cs
@@ -0,0 +1,12 @@
+﻿using PureMVC.Patterns.Facade;
+
+namespace PureMVC.WinFormApp
+{
+    public class ApplicationFacade : Facade
+    {
+        public void Startup()
+        {
+            SendNotification("Startup");
+        }
+    }
+}
